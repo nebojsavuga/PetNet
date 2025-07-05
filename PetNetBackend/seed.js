defce537ffab0482daf7b945005958a2ebd907c6
Nebojša Vuga <nebojsavuga01@gmail.com>
2025-07-05 19:17:37 +0200

    updated seed to use real wallet

--- PetNetBackend/seed.js
+++ PetNetBackend/seed.js
@@ -6,7 +6,7 @@ const User = require('./models/User');
 async function seedUser() {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
-
+        await User.deleteMany({});
         const user = new User({
             fullName: 'Nebojsa Vuga',
             email: 'nebojsavuga01@gmail.com',
@@ -17,11 +17,11 @@ async function seedUser() {
                 latitude: 44.998617,
                 longitude: 19.563195
             },
-            walletAddress: 'abcdabcdabcd',
+            walletAddress: '2kPDhXqAuGGUinUx2se1qkF7qM5N8btwuHKgQdZHYmwK',
         });
 
         await user.save();
-        console.log('User seeded:', user);
+        console.log('User seeded.');
         process.exit(0);
     } catch (err) {
         console.error('Error seeding user:', err.message);
