073883f85d79212eccf3b5309796ef4be5902b2c
Nebojša Vuga <nebojsavuga01@gmail.com>
2025-07-05 19:16:13 +0200

    removed cors

--- PetNetBackend/index.js
+++ PetNetBackend/index.js
@@ -1,6 +1,4 @@
 const express = require('express');
-// TODO
-//const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./config/db');
 
@@ -9,12 +7,6 @@ const authRoutes = require('./routes/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// TODO
-// app.use(cors({
-//     origin: process.env.ORIGIN,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization']
-// }));
 app.use(express.json());
 
 //Database
