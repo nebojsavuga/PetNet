a7ec9f9845d59ff2c4dbe1c73bcea6c0bf39940e
Nebojša Vuga <nebojsavuga01@gmail.com>
2025-07-13 10:22:25 +0200

    removed unused projecty

--- PetNetFrontend/constants/AppConfig.ts
+++
@@ -1,4 +0,0 @@
-export const APP_NAME = 'PetNet';
-export const APP_URL = 'https://petnet.ai';
-export const REDIRECT_URI = 'petnetfrontend://step1';
-export const PHANTOM_CLUSTER = 'devnet';
--- PetNetFrontend/constants/Images.ts
+++
@@ -1,5 +0,0 @@
-export const Images = {
-    metamask: require('../assets/images/metamask-icon.png'),
-    phantom: require('../assets/images/Phantom-Icon_Transparent_White.png'),
-    solflare: require('../assets/images/Solflare_id5j73wBTF_0.png'),
-};
